3e4fd16bcdfae2e7c9618ea144cb5489555f09b5
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-04-18 17:56:37 +0200

    Tutorial 1, extending the Hello World sample.

---
+++ symfony-tutorial-1/src/Controller/StaticController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @Route("/static/", name="home_static")
+     */
+    public function home()
+    {
+        return $this->render('static/index.html.twig', ['controller_name' => 'StaticController/home']);
+    }
+
+    /**
+     * @Route("/static/{pageUrl}", name="static_page")
+     */
+    public function staticPage($pageUrl)
+    {
+        return $this->render('static/'.$pageUrl.'.html.twig', ['controller_name' => 'StaticController/staticPage']);
+    }
+
+    /**
+     * @Route("/{pageUrl}", name="static_page_other")
+     */
+    public function staticPageOther($pageUrl)
+    {
+        return $this->render('static/'.$pageUrl.'.html.twig', ['controller_name' => 'StaticController/staticPageOther']);
+    }
+}
