58d40675cb4ab0be5db9aeedaa7aa96aeead5421
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-04-29 23:57:11 +0200

    Tutorial 5, maker installed.

--- symfony-tutorial-5/config/bundles.php
+++ symfony-tutorial-5/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
