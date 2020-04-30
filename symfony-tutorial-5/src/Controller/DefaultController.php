d2c26247f0a6f71116dedfbf60b666c0d3954d96
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-04-30 23:49:35 +0200

    Tutorial 5, create entity Address.

--- symfony-tutorial-5/src/Controller/DefaultController.php
+++ symfony-tutorial-5/src/Controller/DefaultController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/ruta-amigable/{parametro}", name="default")
-     */
-    public function index()
+    * @Route("/ruta-amigable/{parametro}", name="default")
+    */
+    public function index($parametro, Request $request, Session $session)
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
---
+++ symfony-tutorial-5/src/Entity/Address.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $phonenumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?string
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(?string $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+}
