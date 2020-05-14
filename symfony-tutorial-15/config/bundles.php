35409319a2da02dd63c109274f799347c422c068
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-05-14 22:35:25 +0200

    Tutorial 15, vendors and initial custom files.

--- symfony-tutorial-15/config/bundles.php
+++ symfony-tutorial-15/config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
