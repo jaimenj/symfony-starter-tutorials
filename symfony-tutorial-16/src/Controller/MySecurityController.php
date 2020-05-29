7f0b7bcd0e2d259bd3c19eb59ee718a4d71d2567
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-05-29 23:15:57 +0200

    Tutorial 16, security controller working.

---
+++ symfony-tutorial-16/src/Controller/MySecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * @Route("/protected")
+ */
+class MySecurityController extends AbstractController
+{
+    private $tokenManager;
+
+    public function __construct(CsrfTokenManagerInterface $tokenManager = null)
+    {
+        $this->tokenManager = $tokenManager;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
+}
