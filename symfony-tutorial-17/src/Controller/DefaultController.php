e0c2d26f5fde40ea3bed1797824859c5bc9e5f62
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-05-30 09:02:32 +0200

    Tutorial 17, retrieving session variables.

--- symfony-tutorial-17/src/Controller/DefaultController.php
+++ symfony-tutorial-17/src/Controller/DefaultController.php
@@ -4,16 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $session = $request->getSession();
+
+        $session->set('city', 'Nombre de la ciudad');
+        $session->set('country_name', 'Nombre del país');
+        $session->set('latitude', 0.123);
+        $session->set('longitude', 0.123);
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/show-session-variables", name="show")
+     */
+    public function show(Request $request)
+    {
+        $detectedLatitude = $request->getSession()->get('latitude');
+
+        return $this->render('default/show.html.twig', [
+            'controller_name' => 'DefaultController',
+            'detected_latitude' => $detectedLatitude,
         ]);
     }
 }
