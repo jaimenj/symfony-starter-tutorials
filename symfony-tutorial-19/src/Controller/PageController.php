2be67938615d44c26c63ca795e1f978169f8ea68
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-06-14 13:38:31 +0200

    Tutorial 19, CRUD for page translations.

---
+++ symfony-tutorial-19/src/Controller/PageController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Page;
+use App\Form\PageType;
+use App\Repository\PageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/{_locale}/page")
+ */
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="page_index", methods={"GET"})
+     */
+    public function index(PageRepository $pageRepository): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'pages' => $pageRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="page_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $page = new Page();
+        $form = $this->createForm(PageType::class, $page);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($page);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('page_index');
+        }
+
+        return $this->render('page/new.html.twig', [
+            'page' => $page,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="page_show", methods={"GET"})
+     */
+    public function show(Page $page): Response
+    {
+        return $this->render('page/show.html.twig', [
+            'page' => $page,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="page_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Page $page): Response
+    {
+        $form = $this->createForm(PageType::class, $page);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('page_index');
+        }
+
+        return $this->render('page/edit.html.twig', [
+            'page' => $page,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="page_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Page $page): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$page->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($page);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('page_index');
+    }
+}
---
+++ symfony-tutorial-19/src/Entity/Page.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PageRepository::class)
+ */
+class Page
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $locale;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $urlKey;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    public function getUrlKey(): ?string
+    {
+        return $this->urlKey;
+    }
+
+    public function setUrlKey(string $urlKey): self
+    {
+        $this->urlKey = $urlKey;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
