9a6c5fd85cde43bba47a463270395af79330d1e8
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2020-12-19 10:30:28 +0100

    Entity Role and CRUD.

---
+++ symfony-tutorial-16b/src/Controller/RoleController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Role;
+use App\Form\RoleType;
+use App\Repository\RoleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/role")
+ */
+class RoleController extends AbstractController
+{
+    /**
+     * @Route("/", name="role_index", methods={"GET"})
+     */
+    public function index(RoleRepository $roleRepository): Response
+    {
+        return $this->render('role/index.html.twig', [
+            'roles' => $roleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="role_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $role = new Role();
+        $form = $this->createForm(RoleType::class, $role);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($role);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('role_index');
+        }
+
+        return $this->render('role/new.html.twig', [
+            'role' => $role,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="role_show", methods={"GET"})
+     */
+    public function show(Role $role): Response
+    {
+        return $this->render('role/show.html.twig', [
+            'role' => $role,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="role_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Role $role): Response
+    {
+        $form = $this->createForm(RoleType::class, $role);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('role_index');
+        }
+
+        return $this->render('role/edit.html.twig', [
+            'role' => $role,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="role_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Role $role): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$role->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($role);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('role_index');
+    }
+}
---
+++ symfony-tutorial-16b/src/Entity/Role.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RoleRepository::class)
+ */
+class Role
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=16, nullable=true)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
